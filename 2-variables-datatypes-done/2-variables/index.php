3a565259f0f95977729ffb9a54fa4456b49e0815
Nick Rabone <n.rabone72@aol.com>
2024-07-21 04:43:01 +0100

    Data Types

--- 2-variables-datatypes-done/2-variables/index.php
+++ 2-variables-datatypes-done/2-variables/index.php
@@ -1,18 +1,7 @@
 <?php
-
-/*
-  PHP DATA TYPES:
-
-- String
-- Integer
-- Float
-- Boolean
-- Array
-- Object
-- NULL
-- Resource
-*/
-
+$title = 'Learn PHP';
+$heading = 'Fundamentals of PHP';
+$rule_5 = 'They are case sensitive. So, `$name` and `$Name` are two different variables.'
 ?>
 
 <!DOCTYPE html>
@@ -28,29 +17,44 @@
   <script src="https://cdn.tailwindcss.com"></script>
   <style>
     body {
+      color: #ff4500;
       font-family: 'Poppins', sans-serif;
       font-weight: 300;
     }
   </style>
-  <title>Learn PHP</title>
+  <title><?= $title ?></title>
 </head>
 
 <body class="bg-[#111]">
   <header class="bg-[#ff4500] text-[#111] p-4">
     <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold">Learn PHP</h1>
+      <h1 class="text-3xl font-semibold"><?= $title ?></h1>
     </div>
   </header>
   <div class="container mx-auto p-2 mt-8">
     <div class="bg-[#87ceeb] text-[#111] rounded-lg shadow-md p-2">
-      <h2 class="text-2xl font-semibold mb-2">Fundamentals of PHP</h2>
+      <h2 class="text-2xl font-semibold mb-2"><?= $heading ?></h2>
       <h4>Topics:</h4>
-      <p><em>Data Types</em></p>
-      <p>Integer, Float, Boolean, Array, Object, NULL, Resource</p>
+      <p><em>Variable Rules</em></p>
+      <ul>
+        <li>All variables are prefixed with a dollar sign ($).</li>
+        <li>They must start with a letter or an underscore.</li>
+        <li>They cannot start with a number.</li>
+        <li>They can only contain letters, numbers, and underscores.</li>
+        <li><?php echo $rule_5 ?></li>
+      </ul>
+      <p><em>Conventions</em></p>
+      <ul>
+        <li>Underscores: $server_name</li>
+        <li>Camel Case: $serverName</li>
+        <li>Pascal Case: $ServerName</li>
+        <li>lowercase: $servername</li>
+      </ul>
+      <p></p>
       <h4>Notes:</h4>
       <p></p>
     </div>
   </div>
 </body>
 
-</html> 
+</html>
